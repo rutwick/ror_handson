8407109432b76b3f5f687bc6bab15ce74f243c7f
Rutwick Gangurde <mail@rutwick.com>
2012-10-30 23:44:05 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RorHandson::Application.config.secret_token = '0e8682ce368c5125fa21cb85e2237939bd1cdb718b954a69507d682d044fa5bd4395b8d8a6e8aadfbea686454de4a4d0628664277cb6dcd04123e4472c89c7c4'
